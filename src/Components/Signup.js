ccf5aa023131d46b01753b34a00974af556e7896
Naman Jain <namanjain15032001@gmail.com>
2022-10-18 20:09:51 +0530

    added frontend layout

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -10,6 +10,7 @@ import './SignUp.css'
 import insta from '../Assets/Instagram.JPG';
 import Alert from '@mui/material/Alert';
 import TextField from '@mui/material/TextField';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 
 export default function Login() {
 
@@ -33,14 +34,22 @@ export default function Login() {
                             Sign up to see photos and videos from your friends.
                         </Typography>
                         {true && <Alert severity="error">This is an error alert — check it out!</Alert>}
-                        <TextField id="outlined-basic" label="Email" variant="outlined" fullWidth={true} margin="dense"/>
-                        <TextField id="outlined-basic" label="Password" variant="outlined" fullWidth={true} margin="dense" />
-                        <TextField id="outlined-basic" label="Full Name" variant="outlined" fullWidth={true} margin="dense" />
+                        <TextField id="outlined-basic" label="Email" variant="outlined" fullWidth={true} margin="dense" size="small" />
+                        <TextField id="outlined-basic" label="Password" variant="outlined" fullWidth={true} margin="dense" size="small" />
+                        <TextField id="outlined-basic" label="Full Name" variant="outlined" fullWidth={true} margin="dense" size="small" />
+                        <Button size="small" fullWidth={true} color='secondary' variant="outlined" margin="dense" startIcon={<CloudUploadIcon />} component="label">Upload Profile Image
+                            <input type="file" accept='image/*' hidden />
+                        </Button>
+
                     </CardContent>
                     <CardActions>
-                        <Button size="small">Share</Button>
-                        <Button size="small">Learn More</Button>
+                        <Button color='primary' fullWidth={true} variant="contained">Sign Up</Button>
                     </CardActions>
+                    <CardContent>
+                        <Typography className={classes.text1} variant="subtitle1">
+                            By Signing Up , you agree to our Terms , Data Policy and Cookies Policy .
+                        </Typography>
+                    </CardContent>
                 </Card>
             </div>
         </div>
