f2d1b7d87785b69aaed88e16b33d7981c24e3ca9
Naman Jain <namanjain15032001@gmail.com>
2022-10-22 12:04:55 +0530

    Added post fn

--- src/Components/Like.js
+++ src/Components/Like.js
@@ -1,13 +1,45 @@
 import React, { useEffect, useState } from 'react'
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import { database } from '../firebase';
 
 function Like({ userData, postData }) {
     const [like, setLike] = useState(null);
 
     useEffect(() => {
-
+        let check = postData.likes.includes(userData.userId) ? true : false;
+        setLike(check);
     }, [postData])
+
+    const handleLike = () => {
+        if (like == true) {
+            let narr = postData.likes.filter((ele) =>
+                ele != userData.userId
+            )
+
+            database.posts.doc(postData.postId).update({
+                likes: narr
+            })
+        } else {
+
+            let narr = [...postData.likes, userData.userId];
+
+            database.posts.doc(postData.postId).update({
+                likes: narr
+            })
+
+        }
+    }
     return (
-        <div>Like</div>
+        <div>{
+            like != null ?
+                <>
+                    {
+                        like == true ? <FavoriteIcon className={`icon-styling like`} onClick={handleLike} /> : <FavoriteIcon className={`icon-styling unlike`} onClick={handleLike} />
+                    }
+                </> :
+                <></>
+        }
+        </div>
     )
 }
 
